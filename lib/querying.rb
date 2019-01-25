1dc0ab51ea872745fcecf58738173bcb1b4c836a
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 01:29:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, series.subgenre_id FROM authors\
   JOIN series
   ON authors.id = series.author_id
-  ";
+  WHERE ";
 end
 
 def select_series_title_with_most_human_characters
