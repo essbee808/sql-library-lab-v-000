5a6af507699a6d5ff3c8d2d58ea989bc80fc2092
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 01:55:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
-  LEFT JOIN subgenres
+  LEFT JOIN series
     ON authors.subgenre_id = subgenres.id
   ";
 end
