ad2a29960fedd1aecf8638c0338abea0280de36a
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 00:42:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,7 +8,7 @@ INSERT INTO books VALUES (4, "Cheshire Cat", 1986, 1);
 INSERT INTO books VALUES (5, "The Deathly Hollows Part 1", 2006, 2);
 INSERT INTO books VALUES (6, "The Deathly Hollows Part 2", 2008, 2);
 
-INSERT INTO characters VALUES (1, "Albus", "Human", "I am the best", 2,  );
+INSERT INTO characters VALUES (1, "Albus", "Human", "I am the best", 2, 2);
 INSERT INTO characters VALUES (1, "", "", "", , );
 INSERT INTO characters VALUES (1, "", "", "", , );
 INSERT INTO characters VALUES (1, "", "", "", , );
