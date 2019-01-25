0052364ef0c7b351b47192c36c092313481239d2
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 00:35:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,11 @@
 INSERT INTO series VALUES (1, "The MadHatter", 2, 1);
 INSERT INTO series VALUES (2, "Alice in Wonderland", 1, 2);
 
+INSERT INTO books VALUES ()
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
