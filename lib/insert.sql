8ccb7dd7400559551100f0865a09c5726fb5fb9c
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 00:36:21 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO series VALUES (1, "The Lord of the Rings", 2, 1);
 INSERT INTO series VALUES (2, "Harry Potter", 1, 2);
 
-INSERT INTO books VALUES (1, "" )
+INSERT INTO books VALUES (1, "The Sorcerer's Stone", 2000,  )
 
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
