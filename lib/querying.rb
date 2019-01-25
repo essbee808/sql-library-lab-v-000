501f9f47e3d242e8dc0cd35602930f2e63df5fb1
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 01:23:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species FROM characters
+  "SELECT characters.species, COUNT(species) FROM characters
   GROUP BY species";
 end
 
