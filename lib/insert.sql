24d34a7c181c43c7b61f470e9d9c3777a681f885
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 00:39:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,3 +7,5 @@ INSERT INTO books VALUES (3, "The Order of the Phoenix", 1995, 2);
 INSERT INTO books VALUES (4, "Cheshire Cat", 1986, 1);
 INSERT INTO books VALUES (5, "The Deathly Hollows Part 1", 2006, 2);
 INSERT INTO books VALUES (6, "The Deathly Hollows Part 2", 2008, 2);
+
+INSERT INTO characters
