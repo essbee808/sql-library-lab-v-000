84ee0ad357e5ffcc71cbfd3eb0c683173f8d65ad
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 00:36:09 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO series VALUES (1, "The Lord of the Rings", 2, 1);
-INSERT INTO series VALUES (2, "Alice in Wonderland", 1, 2);
+INSERT INTO series VALUES (2, "Harry Potter", 1, 2);
 
 INSERT INTO books VALUES (1, "" )
 
