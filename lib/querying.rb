637c869040692861427849e1c4db98dbb1620f2a
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 01:13:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters
- WHERE length(characters.motto) = SELECT(MAX(length(motto))";
+ WHERE length(characters.motto) = SELECT(MAX(length(motto)) FROM characters";
 end
 
 
