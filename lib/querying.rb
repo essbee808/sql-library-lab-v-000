cae33bbc4de8bab65f039e38110c344b50c5048c
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 01:59:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters
-  WHERE species = 'human'"
+  WHERE species = 'human'
+  ORDER BY series"
 end
 
 def select_character_names_and_number_of_books_they_are_in
