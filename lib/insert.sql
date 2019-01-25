004a6413f0b572d2a35e2b96f2ab7d690580e2df
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 00:39:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO series VALUES (1, "The Lord of the Rings", 2, 1);
 INSERT INTO series VALUES (2, "Harry Potter", 1, 2);
 
-INSERT INTO books VALUES (1, "The Sorcerer's Stone", 2000, 2)
-INSERT INTO books VALUES (2, "Crazy Wildcat", 2002, 1)
-INSERT INTO books VALUES (3, "The Order of the Phoenix", 1995, 2)
-INSERT INTO books VALUES (4, "Cheshire Cat", 1986, 1)
-INSERT INTO books VALUES (5, "The Deathly Hollows Part 1", 2006, 2)
-INSERT INTO books VALUES (6, "The Deathly Hollows Part 2", 2008, 2)
+INSERT INTO books VALUES (1, "The Sorcerer's Stone", 2000, 2);
+INSERT INTO books VALUES (2, "Crazy Wildcat", 2002, 1);
+INSERT INTO books VALUES (3, "The Order of the Phoenix", 1995, 2);
+INSERT INTO books VALUES (4, "Cheshire Cat", 1986, 1);
+INSERT INTO books VALUES (5, "The Deathly Hollows Part 1", 2006, 2);
+INSERT INTO books VALUES (6, "The Deathly Hollows Part 2", 2008, 2);
