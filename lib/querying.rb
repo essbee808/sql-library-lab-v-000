ced5afb554185752fbfcc37398a6bdd3732c4fe8
Sheena Bonilla <sheena.myb@gmail.com>
2019-01-25 01:20:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) FROM characters
-  GROUP BY species";
+  GROUP BY species
+  ORDER BY ";
 end
 
 def select_name_and_series_subgenres_of_authors
